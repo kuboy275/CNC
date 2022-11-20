f10a9b9bcdd9763b27d48e12321a51b33f3803bd
= <=>
2022-11-20 23:21:30 +0700

    Add Function Suggestion Text Random Color

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,15 @@ $(document).ready(function() {
     $('.js-click-color').on('click',function(e){
 
         var classNameColor = $(this).data('name');
+        var imageColor = $(this).parent().attr('data-image') ? $(this).parent().data('image') : false;
+
+        var urlImage = './assets/images/color-image/';
+
+        if (imageColor != false) {
+            $('.image-by-color img').attr('src',urlImage + imageColor);
+        } else {
+            $('.image-by-color img').attr('src','./assets/images/color-image/pink.jpg');
+        }
 
         textShow.removeClass( function(index,className){
             return (className.match (/(^|\s)shadow-\S+/g) || []).join('');
@@ -33,10 +42,26 @@ $(document).ready(function() {
         textShow.addClass('shadow-'+ classNameColor );
         $('.js-click-color').find('span').css('color','');
         $(this).find('span').css('color',$(this).data('value'));
+        
     });
-  
+
+
+    var ele =  $('.text-suggestion span');
+    for (var i = 0; i < ele.length; i++) {
+        ele.eq(i).addClass('shadow-' + get_random());
+    }
+
 });
 
 function changeFontSize(element,sizeValue) {
     element.css('font-size',sizeValue + 'px');
+}
+
+function get_random () {
+    var arr = [];
+    $('.js-click-color').each(function(i,val){
+        var color = $(val).data('name');
+        arr.push(color);
+    })
+    return arr[Math.floor((Math.random()*arr.length))];
 }
